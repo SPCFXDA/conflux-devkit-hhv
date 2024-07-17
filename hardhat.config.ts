ac84ee740c76a946931a6e7326588ac5f31aea04
SP <spcfxda@gmail.com>
2024-07-17 12:06:16 +0000

    WIP

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,9 +7,12 @@ import "@nomicfoundation/hardhat-ignition-viem";
 import "@nomicfoundation/hardhat-viem";
 
 import { readFileSync } from "fs";
-const secret = `0x${readFileSync("/opt/conflux/genesis_secrets.txt", "utf-8").split(/\r?\n/)[0]}`;
+const genesis = readFileSync("/opt/conflux/genesis_secrets.txt", "utf-8")
+  .split(/\r?\n/)
+  .filter(line => line.length > 0)
+  .map(line => `0x${line}`);
 
-const deployerPrivateKey = process.env.DEPLOYER_PRIVATE_KEY ?? secret;
+const deployerPrivateKey = process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : genesis;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
@@ -19,27 +22,27 @@ const config: HardhatUserConfig = {
     // If the network you are looking for is not here you can add new network settings
     confluxCoreLocal: {
       url: "http://localhost:12537",
-      accounts: [deployerPrivateKey],
+      accounts: deployerPrivateKey,
     },
     confluxCoreTestnet: {
       url: "https://test.confluxrpc.com",
-      accounts: [deployerPrivateKey],
+      accounts: deployerPrivateKey,
     },
     confluxCore: {
       url: "https://main.confluxrpc.com",
-      accounts: [deployerPrivateKey],
+      accounts: deployerPrivateKey,
     },
     confluxESpaceLocal: {
       url: "http://localhost:8545",
-      accounts: [deployerPrivateKey],
+      accounts: deployerPrivateKey,
     },
     confluxESpaceTestnet: {
       url: "https://evmtestnet.confluxrpc.com",
-      accounts: [deployerPrivateKey],
+      accounts: deployerPrivateKey,
     },
     confluxESpace: {
       url: "https://evm.confluxrpc.com",
-      accounts: [deployerPrivateKey],
+      accounts: deployerPrivateKey,
     },
   },
 };
